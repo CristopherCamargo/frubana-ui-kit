40c25fbb5c9d36bedd58ae530dc5b7f1d8f794ad
Cristopher Caceres <cristopher@frubana.co>
2020-02-06 18:03:22 -0500

    add image, input components

---
+++ src/input/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const InputWrapper = styled.input`
+  padding: 8px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px 0 rgba(155, 155, 155, 0.25);
+  background-color: #ffffff;
+  :focus {
+    outline: none;
+  }
+`;
+
+export { InputWrapper };
